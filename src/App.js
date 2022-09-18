ce4b0da0c507b98d9e1be21a414b5df6f9668b94
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-18 13:00:39 +0300

    v-2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 
 import './App.css';
 import Button from './components/Button';
+import Input from "./components/Input";
 
 function App() {
   return (
     <div className="App">
       <div className="calc-wrapper">
+        <Input />
         <div className="row">
           <Button symbol="7" />
           <Button symbol="8" />
           <Button symbol="9" />
-          <Button symbol="/" />
+          <Button symbol="/" color="#00ADB5" />
         </div>
 
         <div className="row">
           <Button symbol="4" />
           <Button symbol="5" />
           <Button symbol="6" />
-          <Button symbol="*" />
+          <Button symbol="*" color="#00ADB5" />
         </div>
 
         <div className="row">
           <Button symbol="1" />
           <Button symbol="2" />
           <Button symbol="3" />
-          <Button symbol="+" />
+          <Button symbol="+" color="#00ADB5" />
         </div>
 
         <div className="row">
           <Button symbol="0" />
           <Button symbol="." />
           <Button symbol="=" />
-          <Button symbol="-" />
+          <Button symbol="-" color="#00ADB5" />
         </div>
+
+        <Button symbol="Clear" />
       </div>
     </div>
   );
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,14 @@ import "./Input.css"
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input-wrapper'>
+        <div className="result">
+            <h1>Result</h1>
+        </div>
+        <div className="text">
+            <h3>Text</h3>
+        </div>
+    </div>
   )
 }
 
